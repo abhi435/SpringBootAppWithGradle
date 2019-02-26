5433e157f0d694dbf8a8951db4e05c699d2d15a0
Aleix Balletbó <aleixballetbo@gmail.com>
2019-02-27 00:02:29 +0100

    New endpoint

--- src/main/java/com/jcombat/controller/DemoController.java
+++ src/main/java/com/jcombat/controller/DemoController.java
@@ -3,13 +3,19 @@ package com.jcombat.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String welcome(Map<String, Object> model) {
+		return "welcome";
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(Map<String, Object> model) {
 		return "welcome";
 	}
 }
